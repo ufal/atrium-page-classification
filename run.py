8bdad4eb8969f957fdde9190398ec2c020406200
k4tel <lutsai.k@gmail.com>
2025-02-09 19:50:21 +0100

    comments and req update

--- run.py
+++ run.py
@@ -7,6 +7,8 @@ if __name__ == "__main__":
     data_dir = f'/lnet/work/people/lutsai/pythonProject/pages/train_final'  # data for training
     test_dir = f'/lnet/work/people/lutsai/pythonProject/pages/test_data'  # considered as default input folder
 
+    def_categ = ["DRAW", "DRAW_L", "LINE_HW", "LINE_P", "LINE_T", "PHOTO", "PHOTO_L", "TEXT", "TEXT_HW", "TEXT_P", "TEXT_T"]
+
     seed = 420
     max_categ = 1400  # max number of category samples
     test_size = 0.1
@@ -14,18 +16,18 @@ if __name__ == "__main__":
 
     epochs = 5  # training only
     batch = 12  # depends on GPU/CPU capabilities
-    top_N = 3
+    top_N = 3  # top N predictions, 3 is enough, 11 for "raw" scores (most scores are 0)
 
     Training = False  # training control
     Testing = False  # evaluation control
 
-    base_model = "google/vit-base-patch16-224"
-    model_folder = "model_1119_3"
-    model_path = f"model/{model_folder}"
+    base_model = "google/vit-base-patch16-224"  # do not change
+    model_folder = "model_1119_3"  # change if needed
+    model_path = f"model/{model_folder}"  # change if needed
 
-    time_stamp = time.strftime("%Y%m%d-%H%M")
+    time_stamp = time.strftime("%Y%m%d-%H%M")  # for results files
 
-    parser = argparse.ArgumentParser(description='Page sorter based on RFC')
+    parser = argparse.ArgumentParser(description='Page sorter based on ViT')
     parser.add_argument('-f', "--file", type=str, default=None, help="Single PNG page path")
     parser.add_argument('-d', "--directory", type=str, default=None, help="Path to folder with PNG pages")
     parser.add_argument('-m', "--model", type=str, default=model_path, help="Path to folder with model")
@@ -38,12 +40,13 @@ if __name__ == "__main__":
 
     load_dotenv()
 
-    cur = Path.cwd() #  directory with this script
-    # locally creating new directory pathes instead of .env variables loaded with mistakes
-    output_dir = Path(os.environ.get('FOLDER_RESULTS', cur / "result"))
     page_images_folder = Path(os.environ.get('FOLDER_PAGES', Path(data_dir)))
     input_dir = Path(os.environ.get('FOLDER_INPUT', Path(test_dir))) if args.directory is None else Path(args.directory)
 
+    cur = Path.cwd()  # directory with this script
+    output_dir = Path(os.environ.get('FOLDER_RESULTS', cur / "result"))
+
+    # locally creating new directory paths instead of .env variables loaded with mistakes
     if not output_dir.is_dir():
         os.makedirs(output_dir)
 
@@ -64,7 +67,7 @@ if __name__ == "__main__":
         classifier = ImageClassifier(checkpoint=base_model, num_labels=len(categories))
 
     else:
-        categories = sorted(os.listdir(data_dir))
+        categories = def_categ
         print(f"Category input directories found: {categories}")
 
         # Initialize the classifier
@@ -104,7 +107,7 @@ if __name__ == "__main__":
 
         rdf["TRUE"] = [categories[i] for i in test_labels]
 
-        rdf.to_csv(f"result/tables/{time_stamp}_{model_folder}_EVAL.csv", sep=",", index=False)
+        rdf.to_csv(f"{output_dir}/tables/{time_stamp}_{model_folder}_TOP-{top_N}_EVAL.csv", sep=",", index=False)
 
         confusion_plot(eval_predictions,
                        test_labels,
@@ -133,6 +136,6 @@ if __name__ == "__main__":
                                 categories,
                                 top_N)
 
-        rdf.to_csv(f"result/tables/{time_stamp}_{model_folder}.csv", sep=",", index=False)
+        rdf.to_csv(f"{output_dir}/tables/{time_stamp}_{model_folder}_TOP-{top_N}.csv", sep=",", index=False)
 
 
